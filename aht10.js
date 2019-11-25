112c72885a78cfa02910065baf03460f293f87c7
Gustav <gustav.karlstrom@icomera.com>
2019-11-25 14:54:33 +0100

    Wip

--- aht10.js
+++ aht10.js
@@ -1,6 +1,59 @@
+/* Copyright (c) 2014 Gustav Karlström. See the file LICENSE for copying permission. */
+/*
+Quick description of my module...
+*/
+
+var C = {
+  ahtAddress : 0x38,
+  sensorCalibrateCmd: [0xE1, 0x08, 0x00],
+  sensorMeasureCmd: [0xAC, 0x33, 0x00],
+  bytesInAMebibyte: 1048576,
+  getRHCmd: true,
+  getTempCmd: false,
+  waterVapor: 17.62,//f
+  barometricPressure: 243.5 //f
+};
+
+function AHT10(pin1,pin2) {
+    this.pin1 = pin1;
+    this.pin2 = pin2;
+
+    i2c.setup({ scl: NodeMCU.D2, sda: NodeMCU.D1, bitrate: 300000 });
+    i2c.writeTo(AHT_address, sensorCalibrateCmd);
+    setTimeout(() => {
+        if ((readStatus() & 0x68) === 0x08) {
+            console.log('Success');
+        } else {
+            console.log('Failure');
+        }
+    }, 500);
+}
+
+/** 'public' constants here */
+// MOD123.prototype.C = {
+//   MY : 0x013,         // description
+//   PUBLIC : 0x0541,    // description
+//   CONSTANTS : 0x023   // description
+// };
+
+/** Put most of my comments outside the functions... */
+MOD123.prototype.foo = function() {
+  // you can use C.PRIVATE
+  // or this.C.PUBLIC
+};
+
+/** Put most of my comments outside the functions... */
+MOD123.prototype.bar = function() {
+};
+
+/** This is 'exported' so it can be used with `require('MOD123.js').connect(pin1,pin2)` */
+exports.connect = function (pin1, pin2) {
+  return new AHT10(pin1, pin2);
+};
+
 const AHT_address = 0x38;
 const i2c = new I2C();
-const sensorCalibrateCmd = [0xE1, 0x08, 0x00];
+const sensorCalibrateCmd = ;
 const sensorMeasureCmd = [0xAC, 0x33, 0x00];
 const getRHCmd = true;
 const getTempCmd = false;
